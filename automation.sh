3247ab88fe3b3d7106ef79441e955dc2ce13e645
root <root@ip-172-31-42-132.us-east-2.compute.internal>
2019-12-30 10:11:50 +0000

    5thupdate

--- automation.sh
+++ automation.sh
@@ -1,6 +1,2 @@
 This is expmle file of shell script of automations
-<<<<<<< HEAD
 automation is the main concept behind schell scripting
-=======
-harsha
->>>>>>> 2f0cf71784152171e9dfe78406f9b1e06e925109
