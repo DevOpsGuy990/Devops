1137aef9360325d69f8306d649e48c4a627b7cae
Harshaaries <HarshaVardhan90193@gmail.com>
2020-09-12 20:30:39 +0530

    modified the file

--- automation.sh
+++ automation.sh
@@ -1,4 +1,5 @@
 This is expmle file of shell script of automations
 automation is the main concept behind schell scripting
 it is used in devops industry very huge manner
+edited by harsha koppoju-devops engineer
 
